a97a9c350f45276f4e371c1616b133b586fe49c7
RamziMammadli <remzimemmedli95@gmail.com>
2022-09-01 21:44:24 +0400

    m player

---
+++ index.js
@@ -0,0 +1,7 @@
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
+const audio = document.querySelector('audio')
+
+const playBtn = document.getElementById('play')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
